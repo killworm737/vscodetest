1fad34c33ce34348a5e16240ffef847e84b49037
Kim chen <kimchen@jourdeness.com.tw>
2016-08-10 13:48:12 +0800

    'edit'

--- extension.js
+++ extension.js
@@ -2,22 +2,36 @@
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "sample" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    
     var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        var startLine = 1;
+
+        var selection = vscode.window.activeTextEditor.selection;
+        var selectedText = vscode.window.activeTextEditor.document.getText(selection);
+
+
+        vscode.window.showInformationMessage(selectedText);
+
+        vscode.window.activeTextEditor.edit((editBuilder) => {
+            pos = new vscode.Position(startLine, 50);
+                        
+            editBuilder.insert(pos, 'textToInsert');                                
+        }).then(() => {
+            console.log('Edit applied!');
+            
+
+        }, (err) => {
+            console.log('Edit rejected!');
+            console.error(err);
+        });
+
+
+
     });
 
     context.subscriptions.push(disposable);
