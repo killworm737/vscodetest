c4b104d47f4a888ac880ab82ffde4d4a10d7b4ce
Kim chen <kimchen@jourdeness.com.tw>
2016-08-11 18:11:29 +0800

    'edit'

--- extension.js
+++ extension.js
@@ -9,18 +9,45 @@ function activate(context) {
     
     var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
 
-        var startLine = 1;
 
-        var selection = vscode.window.activeTextEditor.selection;
+        var selection = vscode.window.activeTextEditor.selection;        
         var selectedText = vscode.window.activeTextEditor.document.getText(selection);
+        
+        var startLine = selection.start.line ;        
+        // var selectedText = vscode.window.activeTextEditor.document.getText(selection);
+        // var selectedText = 'function checknegativeStock($stockid,$is_inventory,$loccode,$sale) {';
 
+        var funcFirst = selectedText.indexOf('function ');
+        var funclast = selectedText.indexOf('(');
+        var braceFirst = selectedText.indexOf('(');
+        var braceLast = selectedText.indexOf(')');
 
-        vscode.window.showInformationMessage(selectedText);
+        funcText = selectedText.slice(funcFirst+9,funclast);
+        braceText = selectedText.slice(braceFirst+1,braceLast);
+
+        var tArr = braceText.split(",");
+
+        textToInsert = '';
+  
+        // 組合字串
+        textToInsert = textToInsert   +  '\t' + '/**' +  '\n' ;     
+        textToInsert = textToInsert   +  '\t' +' * [' + funcText + ' description]' +  '\n' ;     
+        tArr.forEach(element => {
+            textToInsert = textToInsert + '\t' + ' * @param [type] '  +  element +   '\n' ;     
+        });  
+        textToInsert = textToInsert + '\t' + ' */' +   '\n';
+
+        // vscode.window.showInformationMessage(selectedText); //顯示提示訊息
 
         vscode.window.activeTextEditor.edit((editBuilder) => {
-            pos = new vscode.Position(startLine, 50);
-                        
-            editBuilder.insert(pos, 'textToInsert');                                
+            pos = new vscode.Position(startLine, 0); 
+
+            if (tArr.length>0) {
+                editBuilder.insert(pos, textToInsert);
+            }
+            
+                   
+                              
         }).then(() => {
             console.log('Edit applied!');
             
